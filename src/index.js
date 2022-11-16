a9f27b3b9371e9be6d6ef33bd78e2c4400d134d6
MRCba <robledomg07@gmail.com>
2022-11-15 21:37:02 -0300

    Added tab-switching logic

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@ import home from "./home.js";
 import menu from "./menu.js";
 import contact from "./contact.js";
 
+let content = document.getElementById("content");
 console.log("All right");
-contact();
+home();
+
+/* Remove everything from the #content div */
+function clearContent() {
+  content.innerHTML = "";
+}
+
+/* Controls the tab-switching logic */
+const tabs = document.querySelectorAll("#home, #menu, #contact");
+console.log(tabs);
+
+tabs.forEach((elem) => {
+  if (elem.id == "home") {
+    elem.addEventListener("click", () => {clearContent(); home();});
+  } else if (elem.id == "menu") {
+    elem.addEventListener("click", () => {clearContent(); menu();});
+  } else if (elem.id == "contact") {
+    elem.addEventListener("click", () => {clearContent(); contact(); });
+  }
+});
